94b584af4547e772b6a86cf9a2570f3fbc66d136
IlGrandeLuca <lucailgrande96@outlook.it>
2021-11-17 14:43:14 +0100

    Update CibiPreferiti.java

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -1,15 +1,62 @@
 package org.generation.italy.array;
 
+import java.util.Scanner;
+
 public class CibiPreferiti {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] favFood = {"Hamburger", "Pollo", "Gamberi", "Piadina", "Pizza", "Ramen", "Polenta"};
-		int ranking = favFood.length;
+		String[] favFood = {"Hamburger", "Pollo", "Gamberi", "Piadina", "Pizza", "Ramen", "Latte", "Polenta"};
+		String[] favFood2 = {"Hamburger", "Pollo", "Gamberi", "Pizza", "Ramen", "Latte", "Polenta"};
+		String midPosition;
+		String topPosition = null;
+		String lastPosition = null;
 		
-		System.out.println("La classifica contiene " + ranking + " elementi.");
-		System.out.println("Primo posto in classifica: " + favFood[ranking - ranking]);
-		System.out.println("Ultimo posto in classifica: " + favFood[ranking - 1]);
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter 0 for even string or 1 for odd string: ");
+		int choose = in.nextInt();
+		int ranking = 0;
+		
+		if (choose == 0) {
+			
+			ranking = favFood.length;
+			topPosition = favFood[ranking - ranking];
+			lastPosition = favFood[ranking - 1];
+			
+		} else if (choose == 1) {
+			
+			ranking = favFood2.length;
+			topPosition = favFood2[ranking - ranking];
+			lastPosition = favFood2[ranking - 1];
+			
+		} else {
+			
+			System.out.println("Error...");
+			
+		}
+		
+		int remainder = ranking % 2;
+		
+		System.out.println("This ranking contains " + ranking + " items.");
+		System.out.println("Top ranking position: " + topPosition);
+		System.out.println("Last ranking position: " + lastPosition);
+		
+		if (remainder == 0) {
+			
+			ranking = (ranking / 2) - 1;
+			midPosition = favFood[ranking];
+			
+		} else {
+			
+			ranking = ranking / 2;
+			midPosition = favFood2[ranking];
+			
+		}
+		
+		System.out.println("Mid ranking position: " + midPosition);
+		
+		in.close();
+		
 	}
 
 }
